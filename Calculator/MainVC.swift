6fd84665e18881bf0ef4fe2ca2833e75344f16b5
etran1102 <30360660+etran1102@users.noreply.github.com>
2017-09-01 23:45:06 -0700

    First Commit

---
+++ Calculator/MainVC.swift
@@ -0,0 +1,28 @@
+//
+//  MainVC.swift
+//  Calculator
+//
+//  Created by Erick Tran on 9/1/17.
+//  Copyright © 2017 Erick Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var outputLbl: UILabel!
+    
+    var runningNumber = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+//
+//    @IBAction func numberPressed(sender: UIButton) {
+//        runningNumber += "\(sender.tag)"
+//        
+//        outputLbl.text = runningNumber
+//    }
+}
+
